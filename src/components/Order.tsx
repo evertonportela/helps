b10aee38dfde05946c4a0249b5488241a5c5c09d
Everton P <evertonportela@outlook.com>
2022-08-14 13:05:21 -0300

    Aula 1

---
+++ src/components/Order.tsx
@@ -0,0 +1,22 @@
+import { VStack, Text } from 'native-base';
+
+export type OrderProps = {
+    id: string;
+    patrimony: string;
+    when: string;
+    status: 'open' | 'closed';
+}
+
+type Props = {
+    data: OrderProps;
+}
+
+export function Order({ data, ...rest }: Props) {
+    return (
+        <VStack>
+            <Text color="white" fontSize="md">
+                Patrimônio {data.patrimony}
+            </Text>
+        </VStack>
+    );
+}
